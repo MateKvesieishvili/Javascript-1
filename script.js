8c96a1cc9173a27743fb72352d26052efe0e55f8
Mate Kvesieishvili <129661796+MateKvesieishvili@users.noreply.github.com>
2023-05-15 17:11:22 +0400

    Homework 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// !problem 1
+var Name = "Mate";
+var Sirname = "Kvesieishvili"
+var Gender = "Male"
+var DateOfBirth = "10.09.2008";
+
+// !problem 2
+
+(100 - (10+15)/5)*3
+
+// *თუარვცდები 3 expression არის
+
+// !problem 3
+var ToBeDevided = 101
+var Divisor = 2
+console.log(101%2)
